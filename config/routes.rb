cb6711d64bcdad62982b4c8a81f32286e908adf5
vanessa massiot <vanessa.massiot@free.fr>
2020-05-13 13:01:19 +0200

    controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants do 
-  	resources :reviews, only: [ :new, :create]
+  	resources :reviews, only: [:new, :create]
   end
+
+  resources :review, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
